94bc2773bef8287e7fb45191f24443b037ceee5f
drwrchrds <drwrchrds@gmail.com>
2014-06-20 15:09:02 -0700

    fix bug with display hours

--- app/assets/javascripts/views/projects/hours_display.js
+++ app/assets/javascripts/views/projects/hours_display.js
@@ -1,19 +1,22 @@
+/*global Freelancer, Backbone, JST */
 Freelancer.Views.HoursDisplay = Backbone.View.extend({
   className: 'panel panel-primary hours-display',
   
   initialize: function() {
-    this.listenTo(this.model, 'change:uninvoiced_hours_count', this.render);
+    this.listenTo(this.model, 
+      'change:total_hours change:uninvoiced_hours_count', this.render);
   },
   template: JST['projects/hours_display'],
   
   render: function() {
-    var disabled = this.model.escape('uninvoiced_hours_count') && this.model.escape('uninvoiced_hours_count') < 1
+    var disabled = !!(this.model.get('uninvoiced_hours_count') && 
+          this.model.get('uninvoiced_hours_count') < 1);
     
     var renderedContent = this.template({
       total_hours: this.model.escape('total_hours'),
       uninvoiced_hours: this.model.escape('uninvoiced_hours_count'),
       disabled: disabled
-    })
+    });
     
     this.$el.html(renderedContent);
     return this;
