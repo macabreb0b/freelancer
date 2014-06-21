3bb0c31747d096e0d703ffa46d7abfa6b4c0ecf6
drwrchrds <drwrchrds@gmail.com>
2014-06-20 19:47:58 -0700

    considerable styling, CSS transforms on menu, logo

--- app/assets/javascripts/views/main_view.js
+++ app/assets/javascripts/views/main_view.js
@@ -1,22 +1,27 @@
 /*global JST, Freelancer, Backbone */
 Freelancer.Views.MainView = Backbone.View.extend({
   initialize: function() {
-    // add sidebar subview and mainview subview
     
     // listenTo 'sidebar' trigger to show which to highlight
+    
+    this.setDisplayHeight();
   },
   
   template: JST['layouts/main'],
   
   render: function() {
-    var renderedContent = this.template({
-      
-    })
+    var renderedContent = this.template()
     // this.attachSubviews();
     var sidebar = new Freelancer.Views.Sidebar();
     this.$el.html(renderedContent);
+    this.setDisplayHeight();
     this.$el.find('#sidebar').html(sidebar.render().$el);
     return this;
+  },
+  
+  setDisplayHeight: function() {
+    var windowHeight = window.innerHeight;
+    this.$('#display').height(windowHeight - 51)
   }
   
   
